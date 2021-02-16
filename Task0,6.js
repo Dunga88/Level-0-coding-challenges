bc7d0c8d55fb6054c5e7e6a188e571c492380504
Dunga88 <77413182+Dunga88@users.noreply.github.com>
2021-02-16 15:12:13 +0200

    Task 0.6

---
+++ Task0,6.js
@@ -0,0 +1,20 @@
+//Task 0.6
+// logic
+const e = parseFloat(console.log("Enter first number: "));
+const f = parseFloat(console.log("Enter second number: "));
+const g = parseFloat(console.log("Enter third number: "));
+let largest;
+
+// check the condition
+if(e >= f && e >= g) {
+    largest = e;
+}
+else if (f >= e && b >= g) {
+    largest = f;
+}
+else {
+    largest = g;
+}
+
+// display the result
+console.log("The Maximum Number Is " + largest);
