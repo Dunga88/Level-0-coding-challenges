d0bd058782fb9dbe1b0a05a3cb1bcb5a708f32f4
Dunga88 <77413182+Dunga88@users.noreply.github.com>
2021-02-19 09:39:24 -0800

    Delete Task0,8.js

--- Task0,8.js
+++
@@ -1,9 +0,0 @@
-//Task 0.8
-function timer(num)
- {
-     // The Math. floor() function returns the largest integer less than or equal to a given number
-   var H = Math. floor(num / 60);
-   var M = num % 60;
-   console.log( H + 'hour/s' + ":" + M + 'minutes/s');
-}
-(timer(71));
