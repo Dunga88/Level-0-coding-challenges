3445b973b7cee96756e02b9b96aa93fd30bcd715
Dunga88 <77413182+Dunga88@users.noreply.github.com>
2021-02-19 09:37:52 -0800

    Delete Task0,5.js

--- Task0,5.js
+++
@@ -1,9 +0,0 @@
-//Task 0.5
-// Using Heron's Formula
-var area = function(A,B,C) {
-	var s = (A+B+C)/2;
-	var a = Math.sqrt(s*(s-A)*(s-B)*(s-C));
-  return a;
-}
-//fill in side lengths below
-console.log(area(4,5,3));
